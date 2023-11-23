fb242a32d6410e5c497371acbd13e652ef55a59f
Everton Ribeiro <everton.nnrb@gmail.com>
2023-11-22 21:59:15 -0400

    setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rest-erudio
+## End points
---
+++ src/main/java/com/erudio/RestErudioApplication.java
@@ -0,0 +1,13 @@
+package com.erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestErudioApplication.class, args);
+	}
+
+}
