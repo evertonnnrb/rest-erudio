b9eb87a9c4f74792839c4442059c9fcc410b243a
Everton Ribeiro <everton.nnrb@gmail.com>
2023-11-22 22:16:34 -0400

    greeting controler gitignore

---
+++ src/main/java/com/erudio/model/Greeting.java
@@ -0,0 +1,15 @@
+package com.erudio.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greeting {
+
+    private Long id;
+
+    public String name;
+}
